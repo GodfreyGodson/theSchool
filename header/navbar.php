11f879b784b661f03956036ad4eae0b39755e11d
GodfreyGodson <81231519+GodfreyGodson@users.noreply.github.com>
2023-05-15 18:53:47 +0300

    Update navbar.php

--- header/navbar.php
+++ header/navbar.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-2 col-lg-3">
-                <!-- <div class="educa-logo-area"><a href="index.html"><img src="assets/images/logo/logo.png" alt="Main Logo"></a></div> -->
+                <!-- <div class="educa-logo-area"><a href="index.html"><img src="assets/images/logo/theschool-logo (2).jpeg" alt="Main Logo"></a></div> -->
             </div>
             <div class="col-md-10 col-lg-9">
                 <nav class="educa-menu-area">
